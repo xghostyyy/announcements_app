688ec2f30896cb4f8645673af9aab60279c1b136
xghostyyy <dihh.online@mail.ru>
2026-06-04 14:00:27 +0300

    Осуществил возможность изменения объявления

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('', views.announcement_list, name='announcement_list'),
+    path('api/v1/trades/', views.announcement_list, name='announcement_list'),
+    path('api/v1/trades/<int:id>/', views.announcement_data, name='announcement_data'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from .models import Announcement, User, Image, Status
+from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json
@@ -65,3 +66,82 @@ def announcement_list(request):
         return JsonResponse(serialized, status=201)
     else:
         return JsonResponse({'error': 'Метод запрещен'}, status=405)
+
+@csrf_exempt 
+def announcement_data(request, id):
+    if request.method == 'GET':
+        announcement = get_object_or_404(Announcement, pk=id)
+
+        image_urls = []
+        for image in announcement.images.all():
+            image_url = request.build_absolute_uri(image.image.url)
+            image_urls.append(image_url)
+
+        data = []
+        data.append({
+            'id': announcement.id,
+            'author': f'Эл.Почта: {announcement.author.email} Телефон: {announcement.author.phone}',
+            'title': announcement.title,
+            'text': announcement.text,
+            'status': announcement.status.name,
+            'created_at': announcement.created_at,
+            'update_at': announcement.update_at,
+            'images_urls': image_urls,
+        })
+        return JsonResponse(data, safe=False, status=200)
+    
+    elif request.method == 'PUT':
+        try:
+            body = json.loads(request.body.decode('utf-8'))
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Неверный JSON'}, status=400)
+        
+        if isinstance(body, list):
+            if len(body) > 0:
+                body = body[0]
+            else:
+                return JsonResponse({'error': 'Пустой массив'}, status=400)
+        elif not isinstance(body, dict):
+            return JsonResponse({'error': 'Некорректный формат данных'}, status=400)
+
+        title = body.get('title', '').strip()
+        text = body.get('text', '').strip()
+        status_id = body.get('status')
+
+        announcement = get_object_or_404(Announcement, pk=id)
+
+        # Убрал, так как сейчас нет возможности авторизоваться под кастомным юзером
+        #if request.user != announcement.author or not request.user.is_admin:
+        #    return JsonResponse({'error': 'Нет прав для редактирования'}, status=403)
+
+        try:
+            status_obj = Status.objects.get(pk=status_id)
+        except (Status.DoesNotExist, TypeError, ValueError):
+            return JsonResponse({'error': 'Статус не найден'}, status=400)
+        
+        if title:
+            if len(title) > 50:
+                return JsonResponse({'error': f'Максимальная длина заголовка 50 символов! (У вас {len(title)} символов)'}, status=400)
+            announcement.title = title
+        if text:
+            announcement.text = text
+        if status_id:
+            announcement.status = status_obj
+
+        announcement.save()
+        
+        serialized = {
+            'id': announcement.id,
+            'status': announcement.status_id, 
+            'title': announcement.title,
+            'text': announcement.text,
+            'author': announcement.author.email,
+            'created_at': announcement.created_at.isoformat(),
+            'update_at': announcement.update_at.isoformat(),
+        }
+
+        return JsonResponse(serialized, status=201)
+    else:
+        return JsonResponse({'error': 'Метод запрещен!'}, status=405)
+
+
